7d616c30673c624d2ac8e24a1dbbc8ef994b0d3d
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-05-12 21:09:53 +0300

    final code

---
+++ script.js
@@ -0,0 +1,33 @@
+function makeNoButtonAppear() {
+    var top = getRandomNumber(400, 500);
+
+    var left =  getRandomNumber(200, 1000);
+
+    document.getElementById("no").style.position = "absolute";
+    document.getElementById("no").style.top = top + "px";
+    document.getElementById("no").style.left = left + "px";
+    document.getElementById("no").style.display = "block";
+   
+  }
+
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
+function appearAfterDelay() {
+    setTimeout(makeNoButtonAppear, Math.random() * 2000);
+
+}
+
+appearAfterDelay();
+
+document.getElementById("no").onclick = function() { 
+    document.getElementById("no").style.display = "none";
+   
+    appearAfterDelay();
+}
+
+document.getElementById("yes").onclick = function() {
+    alert("I Love you too (●'◡'●) 😙❤️");
+}
